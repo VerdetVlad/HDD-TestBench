03afe59de4c3ce76796ceacdad8996c060057361
Vlad Verdet <v.verdet@yahoo.ro>
2020-05-19 23:50:09 +0300

    FileReader completed

--- src/main/java/bench/FileReader.java
+++ src/main/java/bench/FileReader.java
@@ -7,20 +7,34 @@ import timer.Timer;
 
 public class FileReader {
 
+
+
+    // value = (fileSize/MB_Size) / (returnReadVal/10^9)
+
+
+
     private static final int KB_SIZE = 1024; // KB
     private static final int MB_SIZE = 1024 * 1024; // MB
+    private static final int SecInNano = (int)Math.pow(10,9);
 
-    private static final int[] bufferSizes={KB_SIZE,  2*KB_SIZE,  4*KB_SIZE,  8*KB_SIZE,  16*KB_SIZE, 32*KB_SIZE, 64*KB_SIZE};
 
-    private Timer timer = new Timer();
+    private static final int[] bufferSizes={ 1*KB_SIZE,  2*KB_SIZE,  4*KB_SIZE,  8*KB_SIZE,  16*KB_SIZE, 32*KB_SIZE,
+                                             64*KB_SIZE, 128*KB_SIZE, 256*KB_SIZE, 512*KB_SIZE, 1*MB_SIZE};
 
-    public long readWithBufferSize(String fileName, int myBufferSize,
-                                     long fileSize) throws IOException
+    private static final int[] fileSizes = {32*KB_SIZE, 64*KB_SIZE, 128*KB_SIZE, 256*KB_SIZE, 512*KB_SIZE, 1*MB_SIZE,
+                                            2*MB_SIZE, 4*MB_SIZE};
+
+    private static Timer timer = new Timer();
+
+
+
+
+
+
+    private static double readWithBufferSize(String fileName, int myBufferSize,
+                                     int fileSize) throws IOException
     {
-
         InputStream folderPath = new FileInputStream(System.getProperty("user.dir") + "\\" + fileName);
-
-
 
 
         // create stream writer with given buffer size
@@ -28,7 +42,7 @@ public class FileReader {
 
         byte[] buffer = new byte[myBufferSize];
         int i = 0;
-        long toRead = fileSize / myBufferSize;
+        int toRead = fileSize / myBufferSize;
         Random rand = new Random();
 
         timer.start();
@@ -39,42 +53,81 @@ public class FileReader {
             i++;
         }
 
-        return timer.stop();
+        return (double)(timer.stop()/SecInNano);
 
     }
 
-  /*  public void streamWriteFixedSize(String[] fileNames,
+
+
+
+
+    public static String streamReadFixedSize(String[] fileNames,
                                      int minIndex, int maxIndex,
                                      int fileIndex) throws IOException {
 
-        System.out.println("Stream write benchmark with fixed file size");
-        int currentBufferSize = MIN_BUFFER_SIZE;
-        String fileName;
-        int counter = 0;
+
+        String result = new String("");
+
+        int fileSize = fileSizes[fileIndex];
 
 
-        while(minIndex<=maxIndex)
-        {
+        double benchScore = 0;
+        int indexDif = maxIndex - minIndex + 1;
+        int i=0;
 
-            minIndex++;
+        for(i=0; i< indexDif; i++) {
+
+            int buffSize = bufferSizes[minIndex+i];
+
+            double timeSec = readWithBufferSize(HDDBench.path.get(i),buffSize,fileSize);
+            double mbPerSec = (fileSize/MB_SIZE) / timeSec;
+            benchScore += mbPerSec;
+
+            result +=  String.format("%.2f",mbPerSec) + " MB/S;";
         }
 
+        benchScore = benchScore/indexDif;
+        result+=String.format("%.2f",benchScore);
 
 
-        while (currentBufferSize <= MAX_BUFFER_SIZE
-                && counter <= maxIndex - minIndex) {
-            fileName = ... prefix + ? + suffix
-            writeWithBufferSize(fileName, currentBufferSize, fileSize);
-            // update buffer size
-			...
-            counter++;
+        return result;
+
+    }
+
+
+
+    public static String streamReadFixedBuffer(String[] fileNames,
+                                              int minIndex, int maxIndex,
+                                              int buffIndex) throws IOException {
+
+
+        String result = new String("");
+
+        int buffSize = bufferSizes[buffIndex];
+
+        double benchScore = 0;
+        int indexDif = maxIndex - minIndex + 1;
+        int i=0;
+
+        for(i=0; i< indexDif; i++) {
+
+            int fileSize = fileSizes[minIndex+i];
+
+            double timeSec = readWithBufferSize(HDDBench.path.get(i),buffSize,fileSize);
+            double mbPerSec = (fileSize/MB_SIZE) / timeSec;
+            benchScore += mbPerSec;
+
+            result +=  String.format("%.2f",mbPerSec) + " MB/S;";
         }
 
-        benchScore /= (maxIndex - minIndex + 1);
-        String partition = filePrefix.substring(0, filePrefix.indexOf(":\\"));
-        System.out.println("File write score on partition " + partition + ": "
-                + String.format("%.2f", benchScore) + " MB/sec");
-    }*/
+        benchScore = benchScore/indexDif;
+        result+=String.format("%.2f",benchScore);
+
+
+        return result;
+
+    }
+
 
 
 
